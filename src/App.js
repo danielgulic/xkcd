087f4bd6e10992924d7434be178d0f864fb6ed6c
Daniel Gulic <danielgulic@gmail.com>
2018-12-15 12:10:51 +1100

    add link to Munroe's wikipedia page

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
       <div style={{ textAlign: 'center', paddingTop: '5%' }}>
         <h1 style={{ fontSize: '500%' }}>XKCD</h1>
         <h3>
-          <a href='https://en.wikipedia.org/wiki/Xkcd'>comics by Randall Munroe </a>&#8226;
+          <a href='https://en.wikipedia.org/wiki/Randall_Munroe'>comics by Randall Munroe </a>&#8226;
           <a href='https://danielgulic.com'> browser by Daniel Gulic</a>
         </h3>
         <ComicController />
