c91626642e74866228bcbcbd8ac10e8b9705edf3
Daniel Gulic <danielgulic@gmail.com>
2018-12-15 12:58:16 +1100

    next comic key

--- src/components/comic/ComicController.js
+++ src/components/comic/ComicController.js
@@ -12,6 +12,7 @@ class ComicController extends Component {
     }
 
     this.showNextComic = this.showNextComic.bind(this);
+    this.handleNextKey = this.handleNextKey.bind(this);
   }
 
   getRandomInt(min, max) {
@@ -26,6 +27,12 @@ class ComicController extends Component {
     let res = await fetch('https://xkcd.now.sh/' + this.getRandomInt(1, this.state.amount));
     let json = await res.json();
     this.setState({ nextComic: json });    
+  }
+
+  handleNextKey(event) {
+    if(event.which === 78) {
+      this.showNextComic();
+    }
   }
 
   async componentDidMount() {
@@ -45,13 +52,20 @@ class ComicController extends Component {
     // get another random comic to be on hold for when it cycles
     res = await fetch('https://xkcd.now.sh/' + this.getRandomInt(1, this.state.amount));
     json = await res.json();
-    this.setState({ nextComic: json });    
+    this.setState({ nextComic: json });
+
+    document.addEventListener('keydown', this.handleNextKey, false);
+  }
+
+  componentWillUnmount(){
+    document.removeEventListener('keydown', this.escFunction, false);
   }
 
   render() {
     return (
       <div style={{ paddingBottom: '5%' }}>
-        <button onClick={this.showNextComic}>Get another random comic!</button>
+        <button onClick={this.showNextComic}>Get another random comic</button>
+        <span> or press <strong>N</strong>!</span>
         {this.state.comic && 
         <Comic comic={this.state.comic} nextComicImg={this.state.nextComic ? this.state.nextComic.img : undefined} />}
       </div>
