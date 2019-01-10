2ebae8af20d26c1b4b7ebf31c4af0292bb9458c9
Daniel <danielgulic@gmail.com>
2019-01-10 16:01:04 +1100

    remove website credit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
       <div style={{ textAlign: 'center', paddingTop: '5%' }}>
         <h1 style={{ fontSize: '500%' }}>XKCD</h1>
         <h3>
-          <a href='https://en.wikipedia.org/wiki/Randall_Munroe'>comics by Randall Munroe </a>&#8226;
+          <a href='https://en.wikipedia.org/wiki/Randall_Munroe'>comics by Randall Munroe </a>
         </h3>
         <ComicController />
       </div>
